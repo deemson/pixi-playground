c82d63f172abb946603032a65984bacbce2aa9e6
Kurkin <deemson.k@gmail.com>
2022-12-11 10:05:56 +0200

    pixi.js added

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
-console.log('hello world')
+import { Application } from 'pixi.js'
+
+const app = new Application({
+  width: 128,
+  height: 128
+})
+document.body.appendChild(app.view as unknown as Node)
